86014b2dc9b3ec22883297232e964da2cf8c0a17
Abhishek <abhishek@linux.com>
2018-02-20 09:52:51 -0500

    Let's review loops

--- day6/review-loop.go
+++ day6/review-loop.go
@@ -7,17 +7,17 @@ var TestCase int
 func main() {
 	fmt.Scan(&TestCase)
 	for i:=1; i<=TestCase ; i++ {
-			var str string
-			fmt.Scan(&str)
-			var even, odd string
-			for i, k := range str {
-				if i%2 == 0 {
-					even += string(k)
-				}
-				if i%2 != 0 {
-					odd += string(k)
-				}
+		var str string
+		fmt.Scan(&str)
+		var even, odd string
+		for i, k := range str {
+			if i%2 == 0 {
+				even += string(k)
 			}
-			fmt.Println(even + " " + odd)
+			if i%2 != 0 {
+				odd += string(k)
+			}
+		}
+	fmt.Println(even + " " + odd)
 	}
 }
